a1820ffe11701a0cdbd833e7691d34b47a0e97ea
Saso505 <osamasalma921@gmail.com>
2025-05-03 01:15:40 +0300

    final

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import 'flowbite';
 
 import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
